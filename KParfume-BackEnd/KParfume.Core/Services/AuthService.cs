3c25631d1c72e80f4c0a8c5d9c660efa93f769fa
katarinaperovic <kataleja22@gmail.com>
2024-09-05 20:53:31 +0200

    Update AuthService.cs

--- KParfume-BackEnd/KParfume.Core/Services/AuthService.cs
+++ KParfume-BackEnd/KParfume.Core/Services/AuthService.cs
@@ -33,38 +33,16 @@ namespace KParfume.Core.Services
             return _tokenGenerator.GenerateAccessToken(user);
         }
 
-        /*        public Result<AuthenticationTokensDto> Register(RegisterDto account)
-                {
-        //          if (account.Role == UserRoleDto.Administrator) return Result.Fail(FailureCode.InvalidArgument);
-                    if (_userRepository.Exists(account.kor_email)) return Result.Fail(FailureCode.NonUniqueUsername);
-                    //UserRole role = UserRole.Client; // UserRole role = account.Role == UserRoleDto.Manager? UserRole.Manager : UserRole.Client;
-
-
-                    try
-                    {
-                        var user = _userRepository.Create(new User(account.kor_email, account.kor_lozinka, (UserRole)account.kor_uloga, account.kor_ime, account.kor_prezime, account.kor_adresa, account.kor_grad, account.kor_pos_br, account.kor_drzava, account.kor_tel, account.kor_fab_id, account.kor_ime_kompanije));
-
-                        return _tokenGenerator.GenerateAccessToken(user);
-                    }
-                    catch (ArgumentException e)
-                    {
-                        return Result.Fail(FailureCode.InvalidArgument).WithError(e.Message);
-                    }
-                }*/
-
         public Result<AuthenticationTokensDto> Register(RegisterDto account)
         {
-            // Check if the email already exists in the system
             if (_userRepository.Exists(account.kor_email))
                 return Result.Fail(FailureCode.NonUniqueUsername);
 
-            // Check if kor_fab_id is provided and valid only if it is not null
             if (account.kor_fab_id.HasValue && !_fabrikaRepository.Exists(account.kor_fab_id.Value))
                 return Result.Fail(FailureCode.InvalidArgument).WithError("Specified factory does not exist.");
 
             try
             {
-                // Create a new user with the information provided, including an optional kor_fab_id
                 var user = new User(
                     account.kor_email,
                     account.kor_lozinka,
@@ -76,7 +54,7 @@ namespace KParfume.Core.Services
                     account.kor_pos_br,
                     account.kor_drzava,
                     account.kor_tel,
-                    account.kor_fab_id,  // This may be null if not provided
+                    account.kor_fab_id, 
                     account.kor_ime_kompanije
                 );
 
