9623e8b2d48397a6c778e57a772387da8ad1eadc
katarinaperovic <kataleja22@gmail.com>
2024-09-07 17:17:45 +0200

    fix

---
+++ KParfume-BackEnd/KParfume.Core/Mappers/KuponProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using KParfume.API.DTOs;
+using KParfume.Core.Domain;
+
+namespace KParfume.Core.Mappers
+{
+    public class KuponProfile : Profile
+    {
+        public KuponProfile()
+        {
+            CreateMap<Kupon, KuponDto>().ReverseMap();
+        }
+
+    }
+}
---
+++ KParfume-BackEnd/KParfume.Core/Mappers/OcenaProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using KParfume.API.DTOs;
+using KParfume.Core.Domain;
+
+namespace KParfume.Core.Mappers
+{
+    public class OcenaProfile : Profile
+    {
+        public OcenaProfile()
+        {
+            CreateMap<Ocena, OcenaDto>().ReverseMap();
+        }
+
+    }
+}
